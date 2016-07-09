d4671fe37e11927986f84d1e894b91617b78c662
AkihikoITOH <akihiko.mus+github@gmail.com>
2016-07-09 21:13:57 +0900

    added tests

--- tests/algorithms.py
+++ tests/algorithms.py
@@ -5,14 +5,17 @@ import unittest
 import os
 import sys
 from simab.arms.normal import NormalArm
-from simab.algorithms.random_choice import Random
+from simab.algorithms.epsilon_first import EpsilonFirst
 from simab.algorithms.epsilon_greedy import EpsilonGreedy
 from simab.algorithms.oracle import Oracle
-from simab.algorithms.epsilon_first import EpsilonFirst
+from simab.algorithms.random_choice import Random
+from simab.algorithms.single_arm import Single
+from simab.algorithms.softmax import Softmax
+from simab.algorithms.ucb1 import UCB1
 
 ROUNDS=1000
 
-class TestRandomArm(unittest.TestCase):
+class TestRandom(unittest.TestCase):
     def test_play(self,):
         arms = []
         for i in range(3, 8):
@@ -35,7 +38,7 @@ class TestRandomArm(unittest.TestCase):
         self.assertEqual(len(algorithm.history), len(arms))
         self.assertEqual(len(algorithm.history[0]), ROUNDS)
 
-class TestEGreedyArm(unittest.TestCase):
+class TestEGreedy(unittest.TestCase):
     def test_play(self,):
         arms = []
         for i in range(3, 8):
@@ -58,7 +61,7 @@ class TestEGreedyArm(unittest.TestCase):
         self.assertEqual(len(algorithm.history), len(arms))
         self.assertEqual(len(algorithm.history[0]), ROUNDS)
 
-class TestEFirstArm(unittest.TestCase):
+class TestEFirst(unittest.TestCase):
     def test_prediction_play(self,):
         arms = []
         for i in range(3, 8):
@@ -79,6 +82,75 @@ class TestOracle(unittest.TestCase):
         for arm in arms:
             arm.predict(1000)
         algorithm = Oracle(arms)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+class TestSoftmax(unittest.TestCase):
+    def test_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        algorithm = Softmax(arms, 0.05)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+    def test_prediction_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        for arm in arms:
+            arm.predict(1000)
+        algorithm = Softmax(arms, 0.05)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+class TestUCB1(unittest.TestCase):
+    def test_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        algorithm = UCB1(arms)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+    def test_prediction_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        for arm in arms:
+            arm.predict(1000)
+        algorithm = UCB1(arms)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+class TestSingle(unittest.TestCase):
+    def test_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        algorithm = Single(arms, 5)
+        for _ in range(ROUNDS):
+            algorithm.play()
+        self.assertEqual(len(algorithm.history), len(arms))
+        self.assertEqual(len(algorithm.history[0]), ROUNDS)
+
+    def test_prediction_play(self,):
+        arms = []
+        for i in range(3, 8):
+            arms.append(NormalArm(0.1*float(i), 0.1))
+        for arm in arms:
+            arm.predict(1000)
+        algorithm = Single(arms)
         for _ in range(ROUNDS):
             algorithm.play()
         self.assertEqual(len(algorithm.history), len(arms))
